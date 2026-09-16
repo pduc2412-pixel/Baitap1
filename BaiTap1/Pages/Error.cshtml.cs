0127a9ae0f963db88f46204fed8e3b6cb8022392
Phạm Minh Đức <pduc2412@gmail.com>
2026-09-16 09:29:10 +0700

    update all code files

---
+++ BaiTap1/Pages/Error.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaiTap1.Pages;
+
+public class ErrorModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ BaiTap1/Pages/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaiTap1.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
