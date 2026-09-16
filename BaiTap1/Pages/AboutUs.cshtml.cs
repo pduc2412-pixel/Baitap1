a8af9d95213bcd2122c026cb075de8781fd74d2c
Phạm Minh Đức <pduc2412@gmail.com>
2026-09-16 09:43:39 +0700

    cap nhat code

---
+++ BaiTap1/Pages/AboutUs.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BaiTap1.Pages;
+
+public class AboutUsModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
